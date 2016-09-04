4f561f7b86e4753ac539f7b6f23b268d5106bc1d
Jared Stemen <jstemen@gmail.com>
2016-09-03 22:39:05 -0700

    converting to bidirectional map

---
+++ bidirectional_map.go
@@ -0,0 +1,63 @@
+package simple_db
+
+type BidirectionalMap struct {
+	keyToValue     map[interface{}]interface{}
+	//Need fast way to retrieve keys to delete
+	valueToKeysMap map[interface{}]map[interface{}]bool
+}
+
+func NewBidirectionalMap() (bm *BidirectionalMap) {
+	bm = new(BidirectionalMap)
+	bm.keyToValue = make(map[interface{}]interface{})
+	bm.valueToKeysMap = make(map[interface{}]map[interface{}]bool)
+	return
+}
+
+func (bdm *BidirectionalMap)Set(key interface{}, value interface{}) {
+	//remove old value
+	oldVal := bdm.keyToValue[key]
+	keyMap, ok := bdm.valueToKeysMap[oldVal]
+	if ok {
+		delete(keyMap, key)
+	}
+	bdm.keyToValue[key] = value
+
+	//insert new value
+	_, ok = bdm.valueToKeysMap[value]
+	if !ok{
+		bdm.valueToKeysMap[value] = make(map[interface{}]bool)
+	}
+	bdm.valueToKeysMap[value][key] = true
+}
+
+func (bdm *BidirectionalMap)GetKeysFromValue(value interface{}) (keys map[interface{}]bool, ok bool) {
+	keys, ok = bdm.valueToKeysMap[value]
+	return
+}
+
+func (bdm *BidirectionalMap)Get(key interface{}) (value interface{}, ok bool) {
+	value, ok = bdm.keyToValue[key]
+	if ok{
+		return
+	}else{
+		return  nil, false
+	}
+}
+
+func (bdm *BidirectionalMap)Delete(key interface{}) {
+	value, ok := bdm.keyToValue[key]
+	if ok {
+		delete(bdm.valueToKeysMap, value)
+	}
+	delete(bdm.keyToValue, key)
+}
+
+func (bdm *BidirectionalMap)DeleteFromValue(val interface{}) {
+	keys, ok := bdm.valueToKeysMap[val]
+	if ok {
+		for ikey, _ := range keys {
+			delete(bdm.keyToValue, ikey)
+		}
+	}
+	delete(bdm.valueToKeysMap, val)
+}
--- transaction.go
+++ transaction.go
@@ -1,8 +1,9 @@
 package simple_db
 
+import "fmt"
+
 type Transaction struct {
-	storMap  map[string]*string
-	inverMap map[string]map[string]bool
+	keyToVal *BidirectionalMap
 	child    *Transaction
 	parent   *Transaction
 }
@@ -11,8 +12,7 @@ Initializes a transaction
  */
 func NewTransaction() *Transaction {
 	t := new(Transaction)
-	t.inverMap = make(map[string]map[string]bool)
-	t.storMap = make(map[string]*string)
+	t.keyToVal = NewBidirectionalMap()
 	return t
 }
 
@@ -32,19 +32,7 @@ name - name key to set
 val - value of key to set
  */
 func (t *Transaction) Set(name string, val string) {
-	//remove old reference in InverMap
-	t.Unset(name)
-
-	//store new
-	t.storMap[name] = &val
-	keyMap, ok := t.inverMap[val]
-	if ok {
-		keyMap[name] = true
-	}else {
-		keyMap = make(map[string]bool)
-		keyMap[name] = true
-		t.inverMap[val] = keyMap
-	}
+	t.keyToVal.Set(name, val)
 }
 
 /**
@@ -75,8 +63,8 @@ func (t *Transaction) Commit() (commitedTrans *Transaction, ok bool) {
 			if parent == nil {
 				return
 			}
-			for k, v := range tran.storMap {
-				parent.Set(k, *v)
+			for k, v := range tran.keyToVal.keyToValue {
+				parent.Set(k.(string), v.(string))
 			}
 			parent.child = nil
 			commitedTrans = parent
@@ -91,14 +79,7 @@ Unsets value in transaction
 name - key that should be unset
  */
 func (t *Transaction) Unset(name string) {
-	oldVal, ok := t.storMap[name]
-	if ok {
-		keyMap, ok := t.inverMap[*oldVal]
-		if ok {
-			delete(keyMap, name)
-		}
-	}
-	t.storMap[name] = nil
+	t.keyToVal.Set(name, nil)
 }
 
 /**
@@ -122,13 +103,16 @@ Counts number of keys that are set to specified value in both immediate and acce
 func (t *Transaction) NumEqualTo(targetVal string) (count int) {
 	acc := make(map[string]bool)
 	t.iterateUp(func(_, tran *Transaction) {
-		keyMap, ok := tran.inverMap[targetVal]
+		keyMap, ok := tran.keyToVal.GetKeysFromValue(targetVal)
+		fmt.Printf("Im looking up %#v\n", targetVal)
+		fmt.Printf("keytoval %#v\n\n", tran.keyToVal.valueToKeysMap)
 		if ok {
 			for key, _ := range keyMap {
+				keyStr := key.(string)
 				//valid that the key is still the same in the current transaction
-				realVal := t.Get(key)
-				if realVal !=nil && *t.Get(key) == targetVal {
-					acc[key] = true
+				realVal := t.Get(keyStr)
+				if realVal != nil && *t.Get(keyStr) == targetVal {
+					acc[keyStr] = true
 				}
 			}
 		}
@@ -141,9 +125,14 @@ func (t *Transaction) NumEqualTo(targetVal string) (count int) {
 Receives key value
  */
 func (t *Transaction) Get(name string) (*string) {
-	v, ok := t.storMap[name]
+	v, ok := t.keyToVal.Get(name)
 	if ok || t.parent == nil {
-		return v
+		strPtr, ok := v.(string)
+		if ok{
+			return &strPtr
+		}else{
+			return nil
+		}
 	}else {
 		return t.parent.Get(name)
 	}
